606c469ab029e034be971241819871fe2fe1a5db
Tom <tom_hejiaxi@163.com>
2021-09-21 22:22:16 +0800

    what-mysql

--- docker/build/mybin/run.d/what-mysql.sh
+++ docker/build/mybin/run.d/what-mysql.sh
@@ -2,4 +2,4 @@
 
 # tcpflow -c -p -i any port 3306| grep -i -E "TRANSACTION|REPLACE|SELECT|UPDATE|DELETE|INSERT|SET|COMMIT|ROLLBACK|CREATE|DROP|ALTER|CALL" 
 # | sed 's%\(.*\)\([.]\{4\}\)\(.*\)%\3%'
-tcpflow -C -p -i any dst port 3306|grep '[^\.\S]'|sed 's%\(.*\)\([.]\{4\}\)\(.*\)%\3%'
+tcpflow -C -p -i any dst port 3306|grep '[^\.\S]'
