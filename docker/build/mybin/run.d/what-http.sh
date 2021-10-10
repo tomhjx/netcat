566f2262c4be92c73612b8d82d08ba81ce8fe70d
Tom <tom_hejiaxi@163.com>
2021-10-10 17:19:03 +0800

    http

--- docker/build/mybin/run.d/what-http.sh
+++ docker/build/mybin/run.d/what-http.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # tcpflow -c -i any -e http
-tshark -i any -d tcp.port==80,http -T fields -e tcp.analysis.ack_rtt -e http.request.host   -e http.request.uri -e http.file_data -e http.request.line -e http.response.line
+tshark -i any -d tcp.port==80,http -T fields -e tcp.analysis.ack_rtt  -e http.request.full_uri -e http.file_data -e http.request.line -e http.response.line
 
 # tshark -i any  -d tcp.port==80,http
 
