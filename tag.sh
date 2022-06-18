8ab730d941de6f6716210d7e8dd4096f188c8500
Tom <tom_hejiaxi@163.com>
2022-06-18 18:02:23 +0800

    tag tool

--- tag.sh
+++ tag.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
-b='7.4.9'
+ver1i=2
 
-tag=$(git describe --tags `git rev-list --tags="7.4.9.*" --max-count=1`)
+ver1s=(0 1 2)
 
+if [[ ${ver1s[@]} =~ $1 ]]; then
+    ver1i=$1
+fi
+
+tag=$(git describe --tags `git rev-list --tags="*" --max-count=1`)
+
+array=(${tag//-/ })
+ver1=${array[0]}
+ver1=(${ver1//./ }) 
+
+let ver1[$ver1i]+=1
+
+tag=$(printf ".%s" "${ver1[@]}")
+tag=${tag:1}
 echo $tag
+git tag $tag && git push origin $tag
 
-array=(${tag//./ }) 
-
-echo ${array[3]}
